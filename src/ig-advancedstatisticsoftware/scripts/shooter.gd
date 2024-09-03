427ddfc2f04e67bf9c28954885433265968a3135
WaterLemur <38868316+WaterLemur@users.noreply.github.com>
2024-09-03 14:36:48 -0500

    crosshair
    dot and cross

--- src/ig-advancedstatisticsoftware/scripts/shooter.gd
+++ src/ig-advancedstatisticsoftware/scripts/shooter.gd
@@ -9,7 +9,6 @@ var d = 1
 func _ready() -> void: 
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	d = delta
@@ -17,10 +16,6 @@ func _process(delta: float) -> void:
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 	# Calculate the rotation based on mouse movement
-		#var rotation_x = event.relative_x * sensitivity
-		#var rotation_y = event.relative_y * sensitivity
-	# Calculate the rotation based on mouse movement
 		player.rotate_y(-event.relative.x * sensitivity * sens_x * d)
 		player.rotate_x(-event.relative.y * sensitivity * sens_x * d)
-		#player.rotation.x = clamp(player.rotation.x, deg_to_rad(-40), deg_to_rad(6))
 		player.rotation.z = 0
